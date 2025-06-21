987525e416ea91c4a611ea1bc8c397f4c9a002da
Rasmita13 <147432464+Rasmita13@users.noreply.github.com>
2025-06-21 17:55:18 +0530

    Update Document.java

--- Week-1/Hands-On-DesignPattern and Principles/FactoryMethod/Document.java
+++ Week-1/Hands-On-DesignPattern and Principles/FactoryMethod/Document.java
@@ -1,1 +1,22 @@
+public interface Document {
+    void open();
+}
+
+class WordDocument implements Document {
+    public void open() {
+        System.out.println("Opening a Word Document...");
+    }
+}
+
+class PdfDocument implements Document {
+    public void open() {
+        System.out.println("Opening a PDF Document...");
+    }
+}
+
+class ExcelDocument implements Document {
+    public void open() {
+        System.out.println("Opening an Excel Document...");
+    }
+}
 
