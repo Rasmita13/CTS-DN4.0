2a4a2e9982e749b452343693e3208b8df63ba1ae
Rasmita13 <147432464+Rasmita13@users.noreply.github.com>
2025-06-21 17:03:37 +0530

    Update SRP.java

--- Week-1/Solid Principles/SRP.java
+++ Week-1/Solid Principles/SRP.java
@@ -1,1 +1,32 @@
+class Staff {
+    private String fullName;
+    private int staffId;
+
+    public Staff(String fullName, int staffId) {
+        this.fullName = fullName;
+        this.staffId = staffId;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public int getStaffId() {
+        return staffId;
+    }
+}
+
+class StaffReportGenerator {
+    public void generateReport(Staff staff) {
+        System.out.println("Staff Report: " + staff.getFullName() + " (ID: " + staff.getStaffId() + ")");
+    }
+}
+
+public class SRP {
+    public static void main(String[] args) {
+        Staff staff = new Staff("Rasmita", 104);
+        StaffReportGenerator report = new StaffReportGenerator();
+        report.generateReport(staff);
+    }
+}
 
