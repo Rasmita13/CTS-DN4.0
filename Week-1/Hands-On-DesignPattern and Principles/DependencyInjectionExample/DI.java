7abc791f107e1d6b3a74cf328a1316e191132a58
Rasmita13 <147432464+Rasmita13@users.noreply.github.com>
2025-06-21 18:52:01 +0530

    Update DI.java

--- Week-1/Hands-On-DesignPattern and Principles/DependencyInjectionExample/DI.java
+++ Week-1/Hands-On-DesignPattern and Principles/DependencyInjectionExample/DI.java
@@ -1,1 +1,9 @@
+public class DI {
+    public static void main(String[] args) {
+      
+        CustomerRepository repository = new CustomerRepositoryImpl();
+        CustomerService service = new CustomerService(repository);
+        System.out.println(service.getCustomer(101));
+    }
+}
 
