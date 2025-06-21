87c5fb83ec49b415c1f7d03626eaebb5b92d66a4
Rasmita13 <147432464+Rasmita13@users.noreply.github.com>
2025-06-21 18:10:21 +0530

    Update AP.java

--- Week-1/Hands-On-DesignPattern and Principles/Adapter Pattern/AP.java
+++ Week-1/Hands-On-DesignPattern and Principles/Adapter Pattern/AP.java
@@ -1,1 +1,10 @@
+public class AP {
+    public static void main(String[] args) {
+        PaymentProcessor paypalProcessor = new PaypalAdapter();
+        PaymentProcessor stripeProcessor = new StripeAdapter();
+
+        paypalProcessor.processPayment(100.0);
+        stripeProcessor.processPayment(250.0);
+    }
+}
 
