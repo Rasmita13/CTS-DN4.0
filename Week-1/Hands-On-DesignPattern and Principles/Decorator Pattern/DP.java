db3fab148ddd76fcda4b843db3f97723f8fd5f88
Rasmita13 <147432464+Rasmita13@users.noreply.github.com>
2025-06-21 18:13:59 +0530

    Update DP.java

--- Week-1/Hands-On-DesignPattern and Principles/Decorator Pattern/DP.java
+++ Week-1/Hands-On-DesignPattern and Principles/Decorator Pattern/DP.java
@@ -1,1 +1,40 @@
+public class DP {
+    public static void main(String[] args) {
+        PaymentProcessor paypalProcessor = new PaypalAdapter();
+        PaymentProcessor stripeProcessor = new StripeAdapter();
+
+        paypalProcessor.processPayment(100.0);
+        stripeProcessor.processPayment(250.0);
+    }
+}
+interface PaymentProcessor {
+    void processPayment(double amount);
+}
+class Paypal {
+    void sendPaypalPayment(double amount) {
+        System.out.println("Processing Paypal payment of $" + amount);
+    }
+}
+
+class Stripe {
+    void charge(double amount) {
+        System.out.println("Charging Stripe payment of $" + amount);
+    }
+}
+
+class PaypalAdapter implements PaymentProcessor {
+    private Paypal paypal = new Paypal();
+
+    public void processPayment(double amount) {
+        paypal.sendPaypalPayment(amount);
+    }
+}
+
+class StripeAdapter implements PaymentProcessor {
+    private Stripe stripe = new Stripe();
+
+    public void processPayment(double amount) {
+        stripe.charge(amount);
+    }
+}
 
