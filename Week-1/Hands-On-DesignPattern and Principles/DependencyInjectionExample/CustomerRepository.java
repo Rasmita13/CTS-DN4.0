bcdabdbaee04b21e47dec27dd14274e8f346e9b5
Rasmita13 <147432464+Rasmita13@users.noreply.github.com>
2025-06-21 18:48:55 +0530

    Update CustomerRepository.java

--- Week-1/Hands-On-DesignPattern and Principles/DependencyInjectionExample/CustomerRepository.java
+++ Week-1/Hands-On-DesignPattern and Principles/DependencyInjectionExample/CustomerRepository.java
@@ -1,1 +1,10 @@
+public interface CustomerRepository {
+    String findCustomerById(int id);
+}
 
+class CustomerRepositoryImpl implements CustomerRepository {
+    @Override
+    public String findCustomerById(int id) {
+        return "Customer with ID " + id + " found.";
+    }
+}
