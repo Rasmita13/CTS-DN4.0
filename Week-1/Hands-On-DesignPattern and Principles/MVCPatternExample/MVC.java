fbfb183750b4b75fd09beaeed84af6cda846c202
Rasmita13 <147432464+Rasmita13@users.noreply.github.com>
2025-06-21 18:45:09 +0530

    Update MVC.java

--- Week-1/Hands-On-DesignPattern and Principles/MVCPatternExample/MVC.java
+++ Week-1/Hands-On-DesignPattern and Principles/MVCPatternExample/MVC.java
@@ -1,1 +1,15 @@
+public class MVC {
+    public static void main(String[] args) {
+        Student model = new Student();
+        model.setId("101");
+        model.setName("John Doe");
+        model.setGrade("A");
+        StudentView view = new StudentView();
+        StudentController controller = new StudentController(model, view);
+        controller.updateView();
+        controller.setStudentName("Jane Smith");
+        controller.setStudentGrade("B");
+        controller.updateView();
+    }
+}
 
