1c9ee1741df38e92dc72b09ac9967a47529dd84b
Rasmita13 <147432464+Rasmita13@users.noreply.github.com>
2025-06-21 18:01:45 +0530

    Update BP.java

--- Week-1/Hands-On-DesignPattern and Principles/Builder Pattern/BP.java
+++ Week-1/Hands-On-DesignPattern and Principles/Builder Pattern/BP.java
@@ -1,1 +1,20 @@
+public class BP {
+    public static void main(String[] args) {
+        Computer gamingRig = new Computer.Builder()
+            .setCpu("Intel i9")
+            .setRam("32GB")
+            .setStorage("2TB SSD")
+            .build();
+
+        Computer officePc = new Computer.Builder()
+            .setCpu("Intel i5")
+            .setRam("8GB")
+            .setStorage("256GB SSD")
+            .build();
+
+      
+        System.out.println("Gaming Rig -> CPU: " + gamingRig.getCpu() + ", RAM: " + gamingRig.getRam() + ", Storage: " + gamingRig.getStorage());
+        System.out.println("Office PC -> CPU: " + officePc.getCpu() + ", RAM: " + officePc.getRam() + ", Storage: " + officePc.getStorage());
+    }
+}
 
