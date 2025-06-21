7acbd25bc164e07257f0f58544926fd00262d3e2
Rasmita13 <147432464+Rasmita13@users.noreply.github.com>
2025-06-21 18:51:00 +0530

    Update CustomerService.java

--- Week-1/Hands-On-DesignPattern and Principles/DependencyInjectionExample/CustomerService.java
+++ Week-1/Hands-On-DesignPattern and Principles/DependencyInjectionExample/CustomerService.java
@@ -1,1 +1,13 @@
+public class CustomerService {
+    private CustomerRepository customerRepository;
+
+
+    public CustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public String getCustomer(int id) {
+        return customerRepository.findCustomerById(id);
+    }
+}
 
