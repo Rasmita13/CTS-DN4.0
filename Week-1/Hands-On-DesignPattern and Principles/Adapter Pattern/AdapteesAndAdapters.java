7682a404f6d96c897406799dac3c6bd6ef521d03
Rasmita13 <147432464+Rasmita13@users.noreply.github.com>
2025-06-21 18:09:17 +0530

    Update AdapteesAndAdapters.java

--- Week-1/Hands-On-DesignPattern and Principles/Adapter Pattern/AdapteesAndAdapters.java
+++ Week-1/Hands-On-DesignPattern and Principles/Adapter Pattern/AdapteesAndAdapters.java
@@ -1,1 +1,27 @@
+class Paypal {
+    void makePayment(double amount) {
+        System.out.println("[Paypal] Payment of $" + amount + " done.");
+    }
+}
+
+class Stripe {
+    void pay(double amount) {
+        System.out.println("[Stripe] Payment of $" + amount + " done.");
+    }
+}
+class PaypalAdapter implements PaymentProcessor {
+    private Paypal paypal = new Paypal();
+
+    public void processPayment(double amount) {
+        paypal.makePayment(amount);
+    }
+}
+
+class StripeAdapter implements PaymentProcessor {
+    private Stripe stripe = new Stripe();
+
+    public void processPayment(double amount) {
+        stripe.pay(amount);
+    }
+}
 
