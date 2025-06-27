d45997852a78705d7ebec4d22e302e8007596e62
Rasmita13 <147432464+Rasmita13@users.noreply.github.com>
2025-06-27 22:46:22 +0530

    Update FileService.java

--- Week-2/Mockito_Advanced Exercises/Exercise3/FileService.java
+++ Week-2/Mockito_Advanced Exercises/Exercise3/FileService.java
@@ -1,1 +1,17 @@
+public class FileService {
+    private FileReader fileReader;
+    private FileWriter fileWriter;
+
+    public FileService(FileReader fileReader, FileWriter fileWriter) {
+        this.fileReader = fileReader;
+        this.fileWriter = fileWriter;
+    }
+
+    public String processFile() {
+        String content = fileReader.read();
+        String processedContent = "Processed: " + content;
+        fileWriter.write(processedContent);
+        return processedContent;
+    }
+}
 
