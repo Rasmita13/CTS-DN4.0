e4e22523f72b60d0ac4b61ae7c8fcc9225de8425
Rasmita13 <147432464+Rasmita13@users.noreply.github.com>
2025-06-27 23:19:35 +0530

    Update UserServiceTest.java

--- Week-2/JUnit Spring Test Exercises/Exercise6/UserServiceTest.java
+++ Week-2/JUnit Spring Test Exercises/Exercise6/UserServiceTest.java
@@ -1,1 +1,35 @@
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(MockitoExtension.class)
+public class UserServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private UserService userService;
+
+    @Test
+    public void testUserNotFoundThrowsException() {
+       
+        Long userId = 99L;
+        when(userRepository.findById(userId)).thenReturn(Optional.empty());
+
+       
+        UserNotFoundException exception = assertThrows(UserNotFoundException.class, () -> {
+            userService.getUserById(userId);
+        });
+
+        assertEquals("User not found with id: 99", exception.getMessage());
+    }
+}
 
