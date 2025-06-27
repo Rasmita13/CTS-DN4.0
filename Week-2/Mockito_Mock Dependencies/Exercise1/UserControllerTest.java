5ab5f3a97f49cccd0c87a4517da5fa4efdfa2091
Rasmita13 <147432464+Rasmita13@users.noreply.github.com>
2025-06-27 23:31:22 +0530

    Update UserControllerTest.java

--- Week-2/Mockito_Mock Dependencies/Exercise1/UserControllerTest.java
+++ Week-2/Mockito_Mock Dependencies/Exercise1/UserControllerTest.java
@@ -1,1 +1,29 @@
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(UserController.class)
+public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserService userService;
+
+    @Test
+    public void testGetUserById() throws Exception {
+        // Arrange
+        User mockUser = new User();
+        mockUser.setId(1L);
+        mockUser.setName("John");
+
+        when(userService.getUserById(1L)).then
 
