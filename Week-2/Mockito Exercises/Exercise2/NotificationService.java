9444b360e7f0f88d0ff4df89bf9ba64219b4f4cf
Rasmita13 <147432464+Rasmita13@users.noreply.github.com>
2025-06-27 20:05:33 +0530

    Update NotificationService.java

--- Week-2/Mockito Exercises/Exercise2/NotificationService.java
+++ Week-2/Mockito Exercises/Exercise2/NotificationService.java
@@ -1,1 +1,15 @@
+package com.example.mockito_demo;
+
+public class NotificationService {
+
+    private final NotificationApi api;
+
+    public NotificationService(NotificationApi api) {
+        this.api = api;
+    }
+
+    public void sendNotification(String msg) {
+        api.send(msg);
+    }
+}
 
