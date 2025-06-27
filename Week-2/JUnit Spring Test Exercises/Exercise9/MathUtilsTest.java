1501563d08ed9d1a6c33db3df763c9be29ba5001
Rasmita13 <147432464+Rasmita13@users.noreply.github.com>
2025-06-27 23:26:49 +0530

    Update MathUtilsTest.java

--- Week-2/JUnit Spring Test Exercises/Exercise9/MathUtilsTest.java
+++ Week-2/JUnit Spring Test Exercises/Exercise9/MathUtilsTest.java
@@ -1,1 +1,18 @@
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MathUtilsTest {
+
+  
+    public boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {2, 4, 6, 8, 10})
+    public void testIsEven(int input) {
+        assertTrue(isEven(input), "Number should be even: " + input);
+    }
+}
 
