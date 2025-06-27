c6c7b9d7775ea0ba28298e9ce7083f82929a31c8
Rasmita13 <147432464+Rasmita13@users.noreply.github.com>
2025-06-27 20:29:25 +0530

    Update MultiService.java

--- Week-2/Mockito Exercises/Exercise5/MultiService.java
+++ Week-2/Mockito Exercises/Exercise5/MultiService.java
@@ -1,1 +1,17 @@
+package com.example.mockito_multi;
+
+public class MultiService {
+    private final ExternalApi api;
+
+    public MultiService(ExternalApi api) {
+        this.api = api;
+    }
+
+    public String getFullStatus() {
+        // Calls the API twice and combines the results
+        String first = api.fetchStatus();
+        String second = api.fetchStatus();
+        return first + " & " + second;
+    }
+}
 
