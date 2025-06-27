3f8d4d909fff0e4f380b2db461fc3b72eff602cd
Rasmita13 <147432464+Rasmita13@users.noreply.github.com>
2025-06-27 22:09:14 +0530

    Update UserServiceTest.java

--- Week-2/Mockito Exercises/Exercise6/UserServiceTest.java
+++ Week-2/Mockito Exercises/Exercise6/UserServiceTest.java
@@ -1,1 +1,27 @@
+package com.example.mockito_order;
+
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InOrder;
+import org.mockito.Mockito;
+
+public class UserServiceTest {
+
+    @Test
+    public void testInteractionOrder() {
+     
+        UserActions mockActions = Mockito.mock(UserActions.class);
+
+    
+        UserService service = new UserService(mockActions);
+        service.executeSession();
+
+       
+        InOrder inOrder = inOrder(mockActions);
+        inOrder.verify(mockActions).login();
+        inOrder.verify(mockActions).loadDashboard();
+        inOrder.verify(mockActions).logout();
+    }
+}
 
