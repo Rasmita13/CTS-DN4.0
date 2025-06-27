a2bab6938e13ca20b41a92a20d88868be4992754
Rasmita13 <147432464+Rasmita13@users.noreply.github.com>
2025-06-27 20:06:32 +0530

    Update NotificationServiceTest.java

--- Week-2/Mockito Exercises/Exercise2/NotificationServiceTest.java
+++ Week-2/Mockito Exercises/Exercise2/NotificationServiceTest.java
@@ -1,1 +1,23 @@
+package com.example.mockito_demo;
+
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+public class NotificationServiceTest {
+
+    @Test
+    public void testSendNotificationCalledWithCorrectArgument() {
+       
+        NotificationApi mockApi = Mockito.mock(NotificationApi.class);
+
+        
+        NotificationService service = new NotificationService(mockApi);
+        service.sendNotification("Hello");
+
+        
+        verify(mockApi).send("Hello");
+    }
+}
 
