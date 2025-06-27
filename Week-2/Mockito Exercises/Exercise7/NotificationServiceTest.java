0f065196fa681574a3c9ea3f9edccb386373d07f
Rasmita13 <147432464+Rasmita13@users.noreply.github.com>
2025-06-27 22:15:18 +0530

    Update NotificationServiceTest.java

--- Week-2/Mockito Exercises/Exercise7/NotificationServiceTest.java
+++ Week-2/Mockito Exercises/Exercise7/NotificationServiceTest.java
@@ -1,1 +1,33 @@
+package com.example.mockito_void_exception;
+
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+public class NotificationServiceTest {
+
+    @Test
+    public void testVoidMethodThrowsException() {
+        MailSender mockSender = Mockito.mock(MailSender.class);
+
+       
+        doThrow(new RuntimeException("Email server down"))
+                .when(mockSender).sendEmail("Important update");
+
+       
+        NotificationService service = new NotificationService(mockSender);
+
+      
+        RuntimeException exception = assertThrows(RuntimeException.class, () -> {
+            service.notifyUser("Important update");
+        });
+
+        assertEquals("Email server down", exception.getMessage());
+
+       
+        verify(mockSender).sendEmail("Important update");
+    }
+}
 
