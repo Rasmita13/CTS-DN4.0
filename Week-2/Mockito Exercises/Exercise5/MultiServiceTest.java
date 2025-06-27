3473ce84cc17c715aedac000820a0fbfa6f20287
Rasmita13 <147432464+Rasmita13@users.noreply.github.com>
2025-06-27 20:31:11 +0530

    Update MultiServiceTest.java

--- Week-2/Mockito Exercises/Exercise5/MultiServiceTest.java
+++ Week-2/Mockito Exercises/Exercise5/MultiServiceTest.java
@@ -1,1 +1,21 @@
+package com.example.mockito_multi;
+
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+public class MultiServiceTest {
+
+    @Test
+    public void testMultipleReturns() {
+        
+        ExternalApi mockApi = Mockito.mock(ExternalApi.class);
+        when(mockApi.fetchStatus()).thenReturn("READY", "PROCESSING");
+        MultiService service = new MultiService(mockApi);
+        String result = service.getFullStatus();
+        assertEquals("READY & PROCESSING", result);
+    }
+}
 
