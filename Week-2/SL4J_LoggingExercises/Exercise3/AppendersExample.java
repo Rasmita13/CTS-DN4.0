78e80fb365d29f1b05d617072363588ce5c7ccf9
Rasmita13 <147432464+Rasmita13@users.noreply.github.com>
2025-06-27 23:46:06 +0530

    Update AppendersExample.java

--- Week-2/SL4J_LoggingExercises/Exercise3/AppendersExample.java
+++ Week-2/SL4J_LoggingExercises/Exercise3/AppendersExample.java
@@ -1,1 +1,13 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AppendersExample {
+    private static final Logger logger = LoggerFactory.getLogger(AppendersExample.class);
+
+    public static void main(String[] args) {
+        logger.info("This is an info message.");
+        logger.warn("This is a warning.");
+        logger.error("This is an error.");
+    }
+}
 
