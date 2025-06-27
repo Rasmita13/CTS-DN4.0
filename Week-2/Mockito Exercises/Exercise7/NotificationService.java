788e0810446d467461903f6af243eb35e1a61a50
Rasmita13 <147432464+Rasmita13@users.noreply.github.com>
2025-06-27 22:13:40 +0530

    Update NotificationService.java

--- Week-2/Mockito Exercises/Exercise7/NotificationService.java
+++ Week-2/Mockito Exercises/Exercise7/NotificationService.java
@@ -1,1 +1,14 @@
+package com.example.mockito_void_exception;
+
+public class NotificationService {
+    private final MailSender sender;
+
+    public NotificationService(MailSender sender) {
+        this.sender = sender;
+    }
+
+    public void notifyUser(String msg) {
+        sender.sendEmail(msg);
+    }
+}
 
