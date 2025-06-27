2b4445d0f31bc6eb2e1e8e4e915ec864ae665f65
Rasmita13 <147432464+Rasmita13@users.noreply.github.com>
2025-06-27 20:24:54 +0530

    Update AppServiceTest.java

--- Week-2/Mockito Exercises/Exercise4/AppServiceTest.java
+++ Week-2/Mockito Exercises/Exercise4/AppServiceTest.java
@@ -1,1 +1,20 @@
+package com.example.mockito_void;
+
+import static org.mockito.Mockito.*;
+import static org.mockito.ArgumentMatchers.anyString;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+public class AppServiceTest {
+
+    @Test
+    public void testVoidMethodInteraction() {
+        
+        Logger mockLogger = Mockito.mock(Logger.class);
+        AppService service = new AppService(mockLogger);
+        service.startProcess();
+        verify(mockLogger).log("Starting process...");
+    }
+}
 
