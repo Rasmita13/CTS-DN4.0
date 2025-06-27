0ad1cadaea81deef10f248da78e94667af8eba91
Rasmita13 <147432464+Rasmita13@users.noreply.github.com>
2025-06-27 22:31:54 +0530

    Update ApiServiceTest.java

--- Week-2/Mockito_Advanced Exercises/Exercise2/ApiServiceTest.java
+++ Week-2/Mockito_Advanced Exercises/Exercise2/ApiServiceTest.java
@@ -1,1 +1,23 @@
+import static org.mockito.Mockito.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ApiServiceTest {
+
+    @Test
+    public void testProcessApiData() {
+      
+        RestClient mockRestClient = mock(RestClient.class);
+
+      
+        when(mockRestClient.fetchDataFromApi()).thenReturn("Mock API Response");
+
+     
+        ApiService apiService = new ApiService(mockRestClient);
+        String result = apiService.processApiData();
+
+  
+        assertEquals("Processed API: Mock API Response", result);
+    }
+}
 
