1991ef5be3d17fd374c6c0c763a9118680b93bf7
Rasmita13 <147432464+Rasmita13@users.noreply.github.com>
2025-06-27 20:17:48 +0530

    Update MessageService.java

--- Week-2/Mockito Exercises/Exercise3/MessageService.java
+++ Week-2/Mockito Exercises/Exercise3/MessageService.java
@@ -1,1 +1,14 @@
+package com.example.mockito_matcher;
+
+public class MessageService {
+    private final MessageApi api;
+
+    public MessageService(MessageApi api) {
+        this.api = api;
+    }
+
+    public void sendGreeting() {
+        api.send("Hi there!");
+    }
+}
 
