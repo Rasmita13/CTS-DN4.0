99c9526baf10ed8879ee1891a9c244515978254c
Rasmita13 <147432464+Rasmita13@users.noreply.github.com>
2025-06-27 23:01:24 +0530

    Update CalculatorServiceTest.java

--- Week-2/JUnit Spring Test Exercises/CalculatorServiceTest.java
+++ Week-2/JUnit Spring Test Exercises/CalculatorServiceTest.java
@@ -1,1 +1,13 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorServiceTest {
+
+    @Test
+    public void testAdd() {
+        CalculatorService calculatorService = new CalculatorService();
+        int result = calculatorService.add(5, 3);
+        assertEquals(8, result);
+    }
+}
 
