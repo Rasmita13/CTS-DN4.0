15b5c2a2a7754ba4d4d95d7ab9e05fed19e45ffa
Rasmita13 <147432464+Rasmita13@users.noreply.github.com>
2025-06-27 23:18:16 +0530

    Update UserControllerPostTest.java

--- Week-2/JUnit Spring Test Exercises/Exercise5/UserControllerPostTest.java
+++ Week-2/JUnit Spring Test Exercises/Exercise5/UserControllerPostTest.java
@@ -1,1 +1,37 @@
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.mockito.Mockito.when;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(UserController.class)
+public class UserControllerPostTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserService userService;
+
+    @Autowired
+    private ObjectMapper objectMapper; // For converting object to JSON
+
+    @Test
+    public void testCreateUser() throws Exception {
+        User user = new User(1L, "Alice");
+
+        when(userService.saveUser(user)).thenReturn(user); // Assume saveUser exists
+
+        mockMvc.perform(post("/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(user)))
+                .andExpect(status().isOk());
+    }
+}
 
