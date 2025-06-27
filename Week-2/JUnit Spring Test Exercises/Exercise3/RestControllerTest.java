d56625c3f9973d5b309b3f41df487bc1af95f7ac
Rasmita13 <147432464+Rasmita13@users.noreply.github.com>
2025-06-27 23:14:58 +0530

    Update RestControllerTest.java

--- Week-2/JUnit Spring Test Exercises/Exercise3/RestControllerTest.java
+++ Week-2/JUnit Spring Test Exercises/Exercise3/RestControllerTest.java
@@ -1,1 +1,33 @@
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.mockito.Mockito.when;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(UserController.class)
+public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserService userService;
+
+    @Test
+    public void testGetUserById() throws Exception {
+        User mockUser = new User(1L, "John Doe");
+        when(userService.getUserById(1L)).thenReturn(mockUser);
+
+        mockMvc.perform(get("/users/1"))
+               .andExpect(status().isOk())
+               .andExpect(jsonPath("$.id").value(1))
+               .andExpect(jsonPath("$.name").value("John Doe"));
+    }
+}
 
