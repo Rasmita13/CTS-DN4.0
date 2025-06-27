4a7b96f60b938aef54dd9810e3d63c13e384cb16
Rasmita13 <147432464+Rasmita13@users.noreply.github.com>
2025-06-27 22:07:08 +0530

    Update UserService.java

--- Week-2/Mockito Exercises/Exercise6/UserService.java
+++ Week-2/Mockito Exercises/Exercise6/UserService.java
@@ -1,1 +1,16 @@
+package com.example.mockito_order;
+
+public class UserService {
+    private final UserActions actions;
+
+    public UserService(UserActions actions) {
+        this.actions = actions;
+    }
+
+    public void executeSession() {
+        actions.login();
+        actions.loadDashboard();
+        actions.logout();
+    }
+}
 
