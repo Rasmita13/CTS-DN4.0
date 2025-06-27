c8d8c9557aa9734786bc5e9d2a6560d10735699d
Rasmita13 <147432464+Rasmita13@users.noreply.github.com>
2025-06-27 19:26:21 +0530

    Update CalculatorTest.java

--- Week-2/JUnit_Basic_Testing_Exercises/CalculatorTest.java
+++ Week-2/JUnit_Basic_Testing_Exercises/CalculatorTest.java
@@ -1,1 +1,12 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+    @Test
+    public void testAdd() {
+        int result = 2 + 3;
+        Assert.assertEquals(5, result);
+    }
+}
 
