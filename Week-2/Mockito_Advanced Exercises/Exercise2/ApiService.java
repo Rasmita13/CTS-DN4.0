3b39ffccd0ea0d323440aef92892ec6af54b0d6a
Rasmita13 <147432464+Rasmita13@users.noreply.github.com>
2025-06-27 22:29:31 +0530

    Update ApiService.java

--- Week-2/Mockito_Advanced Exercises/Exercise2/ApiService.java
+++ Week-2/Mockito_Advanced Exercises/Exercise2/ApiService.java
@@ -1,1 +1,13 @@
+public class ApiService {
+    private RestClient restClient;
+
+    public ApiService(RestClient restClient) {
+        this.restClient = restClient;
+    }
+
+    public String processApiData() {
+        String data = restClient.fetchDataFromApi();
+        return "Processed API: " + data;
+    }
+}
 
