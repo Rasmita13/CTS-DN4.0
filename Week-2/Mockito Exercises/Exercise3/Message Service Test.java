597f0dbc3512522257caa35aa998495d848b7c3d
Rasmita13 <147432464+Rasmita13@users.noreply.github.com>
2025-06-27 20:19:39 +0530

    Update Message Service Test.java

--- Week-2/Mockito Exercises/Exercise3/Message Service Test.java
+++ Week-2/Mockito Exercises/Exercise3/Message Service Test.java
@@ -1,1 +1,34 @@
+package com.example.mockito_matcher;
+
+import static org.mockito.Mockito.*;
+import static org.mockito.ArgumentMatchers.*;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+public class MessageServiceTest {
+
+    @Test
+    public void testSendWithAnyString() {
+        
+        MessageApi mockApi = Mockito.mock(MessageApi.class);
+
+        
+        MessageService service = new MessageService(mockApi);
+        service.sendGreeting();
+
+        
+        verify(mockApi).send(anyString()); 
+    }
+
+    @Test
+    public void testSendWithStringStartingWithHi() {
+        MessageApi mockApi = Mockito.mock(MessageApi.class);
+        MessageService service = new MessageService(mockApi);
+        service.sendGreeting();
+
+        // Use custom argument matcher (lambda)
+        verify(mockApi).send(argThat(msg -> msg.startsWith("Hi")));
+    }
+}
 
