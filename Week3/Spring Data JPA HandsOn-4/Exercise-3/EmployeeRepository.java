fb84ca198e3c6a450045c8494836aec338efcc6f
Rasmita13 <147432464+Rasmita13@users.noreply.github.com>
2025-07-04 19:26:57 +0530

    Update EmployeeRepository.java

--- Week3/Spring Data JPA HandsOn-4/Exercise-3/EmployeeRepository.java
+++ Week3/Spring Data JPA HandsOn-4/Exercise-3/EmployeeRepository.java
@@ -1,1 +1,16 @@
+package com.example.employeemanagementsystem.repository;
+
+import com.example.employeemanagementsystem.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    // Derived query methods
+    List<Employee> findByDepartmentId(Long departmentId);
+
+    List<Employee> findByFirstNameContainingIgnoreCase(String keyword);
+
+    List<Employee> findBySalaryGreaterThan(Double amount);
+}
 
