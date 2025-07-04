ac0ecf31cb48c70eb671083358a0ad7db9fc67ab
Rasmita13 <147432464+Rasmita13@users.noreply.github.com>
2025-07-04 19:26:06 +0530

    Update DepartmentRepository.java

--- Week3/Spring Data JPA HandsOn-4/Exercise-3/DepartmentRepository.java
+++ Week3/Spring Data JPA HandsOn-4/Exercise-3/DepartmentRepository.java
@@ -1,1 +1,14 @@
+package com.example.employeemanagementsystem.repository;
+
+import com.example.employeemanagementsystem.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+
+    // Derived query methods
+    Optional<Department> findByName(String name);
+
+    boolean existsByName(String name);
+}
 
