260715e6d1ca4cfd760446513cebf826d5e10389
Rasmita13 <147432464+Rasmita13@users.noreply.github.com>
2025-07-04 19:29:46 +0530

    Update EmployeeController.java

--- Week3/Spring Data JPA HandsOn-4/Exercise-4/EmployeeController.java
+++ Week3/Spring Data JPA HandsOn-4/Exercise-4/EmployeeController.java
@@ -1,1 +1,53 @@
+package com.example.employeemanagementsystem.controller;
+
+import com.example.employeemanagementsystem.entity.Employee;
+import com.example.employeemanagementsystem.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    // Create
+    @PostMapping
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    // Read all
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    // Read by ID
+    @GetMapping("/{id}")
+    public Optional<Employee> getEmployeeById(@PathVariable Long id) {
+        return employeeRepository.findById(id);
+    }
+
+    // Update
+    @PutMapping("/{id}")
+    public Employee updateEmployee(@PathVariable Long id, @RequestBody Employee employeeDetails) {
+        Employee employee = employeeRepository.findById(id).orElseThrow();
+        employee.setFirstName(employeeDetails.getFirstName());
+        employee.setLastName(employeeDetails.getLastName());
+        employee.setSalary(employeeDetails.getSalary());
+        employee.setDepartment(employeeDetails.getDepartment());
+        return employeeRepository.save(employee);
+    }
+
+    // Delete
+    @DeleteMapping("/{id}")
+    public void deleteEmployee(@PathVariable Long id) {
+        employeeRepository.deleteById(id);
+    }
+}
 
