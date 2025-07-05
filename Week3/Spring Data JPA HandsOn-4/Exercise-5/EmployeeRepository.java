7c85954a9d557dc056513d08a4fc4691e5ffac4e
Rasmita13 <147432464+Rasmita13@users.noreply.github.com>
2025-07-05 17:53:43 +0530

    Update EmployeeRepository.java

--- Week3/Spring Data JPA HandsOn-4/Exercise-5/EmployeeRepository.java
+++ Week3/Spring Data JPA HandsOn-4/Exercise-5/EmployeeRepository.java
@@ -1,1 +1,17 @@
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    List<Employee> findByDepartmentName(String departmentName);
+    List<Employee> findBySalaryGreaterThan(Double salary);
+    List<Employee> findByNameContaining(String keyword);
+}
+@Query("SELECT e FROM Employee e WHERE e.salary > :minSalary")
+List<Employee> getEmployeesWithSalaryAbove(@Param("minSalary") Double minSalary);
+@Query("SELECT e FROM Employee e WHERE e.department.name = :deptName")
+List<Employee> getEmployeesByDepartment(@Param("deptName") String departmentName);
+
+
 
