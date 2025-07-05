32435f3104e0c0d89b9fff356411df1ba811c609
Rasmita13 <147432464+Rasmita13@users.noreply.github.com>
2025-07-05 19:50:30 +0530

    Update BookRepository.java

--- Week3/Spring Core_Maven/Exercise-5/BookRepository.java
+++ Week3/Spring Core_Maven/Exercise-5/BookRepository.java
@@ -1,1 +1,8 @@
+package com.library;
+
+public class BookRepository {
+    public void saveBook(String bookName) {
+        System.out.println("Book '" + bookName + "' saved to repository.");
+    }
+}
 
