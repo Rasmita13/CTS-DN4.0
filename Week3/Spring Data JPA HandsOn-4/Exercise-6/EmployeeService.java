4a1cdc50d576b239099783650767b7897e0610f6
Rasmita13 <147432464+Rasmita13@users.noreply.github.com>
2025-07-05 18:07:43 +0530

    Update EmployeeService.java

--- Week3/Spring Data JPA HandsOn-4/Exercise-6/EmployeeService.java
+++ Week3/Spring Data JPA HandsOn-4/Exercise-6/EmployeeService.java
@@ -1,1 +1,23 @@
+package com.example.employeemanagement.service;
+import com.example.employeemanagement.entity.Employee;
+import com.example.employeemanagement.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.*;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    public Page<Employee> getEmployees(int page, int size, String sortBy, String sortDir) {
+        Sort sort = sortDir.equalsIgnoreCase("asc")
+                ? Sort.by(sortBy).ascending()
+                : Sort.by(sortBy).descending();
+
+        Pageable pageable = PageRequest.of(page, size, sort);
+        return employeeRepository.findAll(pageable);
+    }
+}
 
