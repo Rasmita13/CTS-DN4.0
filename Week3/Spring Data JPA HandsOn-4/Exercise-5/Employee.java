0ef7c1f0a52b01d0efb1d8c4df61ee14a5080002
Rasmita13 <147432464+Rasmita13@users.noreply.github.com>
2025-07-05 17:56:28 +0530

    Update Employee.java

--- Week3/Spring Data JPA HandsOn-4/Exercise-5/Employee.java
+++ Week3/Spring Data JPA HandsOn-4/Exercise-5/Employee.java
@@ -1,1 +1,22 @@
+import jakarta.persistence.*;
+@Entity
+@NamedQueries({
+    @NamedQuery(name = "Employee.findByExactName",
+                query = "SELECT e FROM Employee e WHERE e.name = :name"),
+
+    
+    @NamedQuery(name = "Employee.findAllSortedBySalary",
+                query = "SELECT e FROM Employee e ORDER BY e.salary DESC")
+})
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Double salary;
+    @ManyToOne
+    private Department department;
+
+   
+}
 
