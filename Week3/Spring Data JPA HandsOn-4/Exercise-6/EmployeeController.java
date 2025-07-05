c90c6edc9d88fd1cd3e71e5c78e59cd9d6a3271e
Rasmita13 <147432464+Rasmita13@users.noreply.github.com>
2025-07-05 18:08:31 +0530

    Update EmployeeController.java

--- Week3/Spring Data JPA HandsOn-4/Exercise-6/EmployeeController.java
+++ Week3/Spring Data JPA HandsOn-4/Exercise-6/EmployeeController.java
@@ -1,1 +1,27 @@
+package com.example.employeemanagement.controller;
+import com.example.employeemanagement.entity.Employee;
+import com.example.employeemanagement.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping
+    public List<Employee> getEmployees(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "5") int size,
+            @RequestParam(defaultValue = "id") String sortBy,
+            @RequestParam(defaultValue = "asc") String sortDir
+    ) {
+        Page<Employee> employeePage = employeeService.getEmployees(page, size, sortBy, sortDir);
+        return employeePage.getContent();
+    }
+}
 
