c1ee62461c531f1f5d4b771bd296c53f7e0d6035
Rasmita13 <147432464+Rasmita13@users.noreply.github.com>
2025-07-05 19:51:11 +0530

    Update BookService.java

--- Week3/Spring Core_Maven/Exercise-5/BookService.java
+++ Week3/Spring Core_Maven/Exercise-5/BookService.java
@@ -1,1 +1,15 @@
+package com.library;
+
+public class BookService {
+    private BookRepository bookRepository;
+
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void addBook(String bookName) {
+        System.out.println("Adding book: " + bookName);
+        bookRepository.saveBook(bookName);
+    }
+}
 
