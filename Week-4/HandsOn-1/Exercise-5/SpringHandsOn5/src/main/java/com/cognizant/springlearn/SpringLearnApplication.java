81bf5bb05e11bec02b74ba0150dd09279321a14e
Rasmita13 <147432464+Rasmita13@users.noreply.github.com>
2025-07-10 14:29:48 +0530

    Add files via upload

---
+++ Week-4/HandsOn-1/Exercise-5/SpringHandsOn5/src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
@@ -0,0 +1,30 @@
+package com.cognizant.springlearn;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+@SpringBootApplication
+public class SpringLearnApplication {
+    private static final Logger LOGGER = LoggerFactory.getLogger(SpringLearnApplication.class);
+
+    public static void main(String[] args) {
+        LOGGER.info("START");
+        SpringApplication.run(SpringLearnApplication.class, args);
+        displayCountry();
+        LOGGER.info("END");
+    }
+
+    public static void displayCountry() {
+        LOGGER.info("START");
+        ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
+        Country country1 = context.getBean("country", Country.class);
+        Country country2 = context.getBean("country", Country.class);
+        LOGGER.debug("Country 1 : {}", country1.toString());
+        LOGGER.debug("Country 2 : {}", country2.toString());
+        LOGGER.info("END");
+    }
+}
