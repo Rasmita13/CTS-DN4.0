eaf3c16a0c90961c2bdf4183f344d79649d3e15a
Rasmita13 <147432464+Rasmita13@users.noreply.github.com>
2025-07-10 16:06:30 +0530

    Update DepartmentDao.java

--- Week-4/HandsOn-3/Exercise-4/DepartmentDao.java
+++ Week-4/HandsOn-3/Exercise-4/DepartmentDao.java
@@ -1,1 +1,16 @@
+package com.example.employees.dao;
+import java.util.List;
+public class DepartmentDao {
+
+    public static List<String> DEPARTMENT_LIST;
+
+    public DepartmentDao(List<String> departmentList) {
+        DEPARTMENT_LIST = departmentList;
+    }
+
+    public List<String> getAllDepartments() {
+        System.out.println("DepartmentDao: Returning department list...");
+        return DEPARTMENT_LIST;
+    }
+}
 
