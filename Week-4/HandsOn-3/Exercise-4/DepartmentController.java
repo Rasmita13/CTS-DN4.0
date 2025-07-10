cd866afe989e1dc1442cddd31a734c803846d426
Rasmita13 <147432464+Rasmita13@users.noreply.github.com>
2025-07-10 16:08:52 +0530

    Update DepartmentController.java

--- Week-4/HandsOn-3/Exercise-4/DepartmentController.java
+++ Week-4/HandsOn-3/Exercise-4/DepartmentController.java
@@ -1,1 +1,25 @@
+package com.example.employees.controller;
+import com.example.employees.service.DepartmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+@RestController
+@RequestMapping("/api")
+public class DepartmentController {
+
+    private final DepartmentService departmentService;
+
+    @Autowired
+    public DepartmentController(DepartmentService departmentService) {
+        this.departmentService = departmentService;
+    }
+
+    @GetMapping("/departments")
+    public List<String> getAllDepartments() {
+        System.out.println("DepartmentController: GET /departments called...");
+        return departmentService.getAllDepartments();
+    }
+}
 
