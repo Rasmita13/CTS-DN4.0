92a008895440565d5837dc520592dc911e9d6ff4
Rasmita13 <147432464+Rasmita13@users.noreply.github.com>
2025-07-10 16:03:17 +0530

    Add files via upload

---
+++ Week-4/HandsOn-3/Exercise-3/employee-rest-service/employee-management/backend/src/main/java/com/example/employees/service/EmployeeService.java
@@ -0,0 +1,23 @@
+package com.example.employees.service;
+
+import com.example.employees.dao.EmployeeDao;
+import com.example.employees.model.Employee;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class EmployeeService {
+
+    private final EmployeeDao employeeDao;
+
+    public EmployeeService(EmployeeDao employeeDao) {
+        this.employeeDao = employeeDao;
+    }
+
+    @Transactional(readOnly = true)
+    public List<Employee> getAllEmployees() {
+        return employeeDao.getAllEmployees();
+    }
+}
