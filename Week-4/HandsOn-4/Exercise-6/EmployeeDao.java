c21284042aa93b1971c3a89ee118606f541fd4a7
Rasmita13 <147432464+Rasmita13@users.noreply.github.com>
2025-07-11 13:56:42 +0530

    Update EmployeeDao.java

--- Week-4/HandsOn-4/Exercise-6/EmployeeDao.java
+++ Week-4/HandsOn-4/Exercise-6/EmployeeDao.java
@@ -1,1 +1,7 @@
+public void deleteEmployee(int id) throws EmployeeNotFoundException {
+    boolean removed = employeeList.removeIf(emp -> emp.getId().equals(id));
+    if (!removed) {
+        throw new EmployeeNotFoundException("Employee with ID " + id + " not found");
+    }
+}
 
