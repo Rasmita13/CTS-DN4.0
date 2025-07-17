191fa6891906983f71972896111126bc4a67425b
Rasmita13 <147432464+Rasmita13@users.noreply.github.com>
2025-07-17 23:41:40 +0530

    Update AuthController.java

--- Week-5/Sample Microservices Exercises/Exercise-3/AuthController.java
+++ Week-5/Sample Microservices Exercises/Exercise-3/AuthController.java
@@ -1,1 +1,27 @@
+package com.example.jwt;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
+    @PostMapping("/login")
+    public Map<String, String> login(@RequestBody Map<String, String> credentials) {
+        String username = credentials.get("username");
+        String password = credentials.get("password");
+
+        if ("user".equals(username) && "password".equals(password)) {
+            String token = jwtTokenProvider.createToken(username);
+            return Map.of("token", token);
+        } else {
+            throw new RuntimeException("Invalid credentials");
+        }
+    }
+}
 
