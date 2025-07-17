873f12bee5ec55ee270fb6babb44391cbed8673e
Rasmita13 <147432464+Rasmita13@users.noreply.github.com>
2025-07-17 21:07:10 +0530

    Update ResilienceConfiguration.java

--- Week-5/Sample Microservices Load Balancing exercises/Exercise-3/ResilienceConfiguration.java
+++ Week-5/Sample Microservices Load Balancing exercises/Exercise-3/ResilienceConfiguration.java
@@ -1,1 +1,31 @@
+package com.example.gateway.config;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCustomizer;
+
+import java.time.Duration;
+
+@Configuration
+public class ResilienceConfiguration {
+
+    @Bean
+    public Resilience4JCustomizer customizer() {
+        return factory -> factory.configureDefault(id ->
+            new Resilience4JConfigBuilder(id)
+                .circuitBreakerConfig(CircuitBreakerConfig.custom()
+                        .failureRateThreshold(50)
+                        .slidingWindowSize(10)
+                        .waitDurationInOpenState(Duration.ofSeconds(10))
+                        .build())
+                .timeLimiterConfig(TimeLimiterConfig.custom()
+                        .timeoutDuration(Duration.ofSeconds(4))
+                        .build())
+                .build()
+        );
+    }
+}
 
