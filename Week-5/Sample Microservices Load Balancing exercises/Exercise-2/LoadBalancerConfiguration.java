77efa58c9cfcaab5a70c01675a97e83020aed144
Rasmita13 <147432464+Rasmita13@users.noreply.github.com>
2025-07-17 20:59:18 +0530

    Update LoadBalancerConfiguration.java

--- Week-5/Sample Microservices Load Balancing exercises/Exercise-2/LoadBalancerConfiguration.java
+++ Week-5/Sample Microservices Load Balancing exercises/Exercise-2/LoadBalancerConfiguration.java
@@ -1,1 +1,26 @@
+package com.example.gateway.config;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClientFactory;
+import org.springframework.cloud.loadbalancer.core.RandomLoadBalancer;
+import org.springframework.cloud.loadbalancer.core.ReactorLoadBalancer;
+import org.springframework.cloud.loadbalancer.support.LoadBalancerClientFactoryBean;
+import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+@Configuration
+public class LoadBalancerConfiguration {
+
+    @Bean
+    public ReactorLoadBalancer<?> randomLoadBalancer(
+            Environment environment,
+            LoadBalancerClientFactory loadBalancerClientFactory) {
+
+        String name = environment.getProperty(LoadBalancerClientFactory.PROPERTY_NAME);
+        return new RandomLoadBalancer(
+                loadBalancerClientFactory.getLazyProvider(name, ServiceInstanceListSupplier.class),
+                name);
+    }
+}
 
